1f23f4e0181fd40cb15e2c5a9d2821c780da327e
YS739 <suk921@gmail.com>
2022-12-12 19:49:40 +0900

    used styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 
-const InputBox = styled.div`
+const InputContainer = styled.div`
+  width: 95%;
+  max-width: 1200px;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  margin: 20px auto;
+`;
+
+const InputBox = styled.input`
+  width: 250px;
+  padding: 15px;
+  border: 1px solid plum;
+
+  :hover {
+    border: 2px solid blue;
+  }
+`;
+
+const AddBtn = styled.button`
+  border: none;
+  border-radius: 20px;
+  padding: 10px 15px;
+  margin-left: 20px;
+
+  background-color: black;
+  color: white;
+
+  cursor: pointer;
+`;
+
+const TodoListContainer = styled.div`
+  width: 95%;
+  max-width: 1200px;
+  height: 450px;
+
+  display: flex;
+  flex-direction: row;
+  overflow-y: scroll;
+
+  background-color: beige;
+`;
+
+const TodoCard = styled.span`
+  width: 200px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 20px;
+  margin-top: 30px;
+
+  border: 2px solid plum;
+  border-radius: 20px;
 `;
 
 const App = () => {
@@ -13,15 +63,15 @@ const App = () => {
   console.log(todoStore);
   return (
     <div>
-      <InputBox>
-        <input type="text" placeholder="Todo의 제목을 입력하세요" />
-        <button>추가하기</button>
-      </InputBox>
-      <div>
-        <div>
+      <InputContainer>
+        <InputBox type="text" placeholder="Todo의 제목을 입력하세요" />
+        <AddBtn>추가하기</AddBtn>
+      </InputContainer>
+      <TodoListContainer>
+        <TodoCard>
           <p>제목</p>
-        </div>
-      </div>
+        </TodoCard>
+      </TodoListContainer>
     </div>
   );
 };
