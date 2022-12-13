8a5bba92a8dc879ca4d52af6921f04da2e091b9f
YS739 <suk921@gmail.com>
2022-12-13 10:33:26 +0900

    split todolistcontainer done

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
-import styled from "styled-components";
-
-import { useSelector } from "react-redux";
 import AddForm from "./components/AddForm";
-
-const TodoListContainer = styled.div`
-  width: 95%;
-  max-width: 1200px;
-  height: 450px;
-
-  display: flex;
-  flex-direction: row;
-  overflow-y: scroll;
-
-  background-color: beige;
-`;
-
-const TodoCard = styled.div`
-  width: 200px;
-  height: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-left: 20px;
-  margin-top: 30px;
-
-  border: 2px solid plum;
-  border-radius: 20px;
-`;
+import TodoListContainer from "./components/TodoListContainer";
 
 const App = () => {
   return (
     <div>
       <AddForm />
-
-      <TodoListContainer>
-        {todoList.map((todos) => (
-          <TodoCard key={todos.id}>{todos.title}</TodoCard>
-        ))}
-      </TodoListContainer>
+      <TodoListContainer />
     </div>
   );
 };
--- src/components/TodoListContainer.jsx
+++ src/components/TodoListContainer.jsx
@@ -1,7 +1,43 @@
+import styled from "styled-components";
+
+import { useSelector } from "react-redux";
+
 const TodoListContainer = () => {
   const { todoList } = useSelector((state) => state.todoList);
 
-  return <div></div>;
+  return (
+    <TodoBox>
+      {todoList.map((todo) => (
+        <TodoCard key={todo.id}>{todo.title}</TodoCard>
+      ))}
+    </TodoBox>
+  );
 };
+
+// styled-components
+const TodoBox = styled.div`
+  width: 95%;
+  max-width: 1200px;
+  height: 450px;
+
+  display: flex;
+  flex-direction: row;
+  overflow-y: scroll;
+
+  background-color: beige;
+`;
+
+const TodoCard = styled.div`
+  width: 200px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 20px;
+  margin-top: 30px;
+
+  border: 2px solid plum;
+  border-radius: 20px;
+`;
 
 export default TodoListContainer;
